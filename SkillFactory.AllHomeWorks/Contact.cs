02db55e1b8a87df2c97a6391b240f8962eb112d0
TMesmeR <mesmerat@gmail.com>
2026-08-15 10:54:18 +0300

    load

---
+++ SkillFactory.AllHomeWorks/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module14._3._3
+{
+    internal class Contact
+    {
+        public Contact(string name, string lastName, long phoneNumber, string email) 
+        {
+            Name = name;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public string Name { get; }
+        public string LastName { get; }
+        public long PhoneNumber { get; }
+        public string Email { get; }
+    }
+}
--- SkillFactory.AllHomeWorks/Program.cs
+++ SkillFactory.AllHomeWorks/Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿using Module14._3._3;
+
+var phoneBook = new List<Contact>();
+
+phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+
+phoneBook = phoneBook.OrderBy(c => c.Name).ThenBy(c => c.LastName).ToList();
+
+
+while (true)
+{
+    Console.WriteLine("Введите номер страницы (1-3):");
+    var input = Console.ReadKey().KeyChar;
+
+    var parsed = Int32.TryParse(input.ToString(), out int pageNumber);
+
+    if (!parsed || pageNumber < 1 || pageNumber > 3)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Страницы не существует");
+    }
+    else
+    {
+        var pageContent = phoneBook.Skip((pageNumber - 1) * 2).Take(2);
+        Console.WriteLine();
+
+        foreach (var entry in pageContent)
+            Console.WriteLine(entry.Name + " " + entry.LastName + ": " + entry.PhoneNumber);
+
+        Console.WriteLine();
+    }
+}
