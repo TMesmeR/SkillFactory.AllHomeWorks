b79e0225fa037a75ff4080356e6dfcf0094341cb
TMesmeR <mesmerat@gmail.com>
2026-08-15 11:10:25 +0300

    load Test

---
+++ SkillFactory.AllHomeWorks.Tests/CalculatorTest.cs
@@ -0,0 +1,67 @@
+﻿using Module16._6._1;
+using NUnit.Framework.Legacy;
+
+namespace SkillFactory.AllHomeWorks.Tests;
+
+public class Tests
+{
+    public class CalculatorTests
+    {
+        private Calculator _calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _calculator = new Calculator();
+        }
+
+        [Test]
+        public void TestAddictional()
+        {
+            int a = 10;
+            int b = 2;
+            int result = _calculator.Additional(a, b);
+
+            ClassicAssert.AreEqual(12, result);
+        }
+
+        [Test]
+        public void TestSubtraction()
+        {
+            int a = 10;
+            int b = 2;
+            int result = _calculator.Subtraction(a, b);
+
+            ClassicAssert.AreEqual(8, result);
+        }
+
+        [Test]
+        public void TestMultiplication()
+        {
+            int a = 10;
+            int b = 2;
+            int result = _calculator.Multiplication(a, b);
+
+            ClassicAssert.AreEqual(20, result);
+        }
+
+        [Test]
+        public void TestDivision()
+        {
+            int a = 10;
+            int b = 2;
+            int result = _calculator.Division(a, b);
+
+            ClassicAssert.AreEqual(5, result);
+        }
+
+        [Test]
+        public void TestDivision_ThrowsDivideByZeroException()
+        {
+            int a = 10;
+            int b = 0;
+
+            Assert.Throws<DivideByZeroException>(() => _calculator.Division(a, b));
+        }
+    }
+}
